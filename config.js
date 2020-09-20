c16896e52169d78bc06a9e0c5bff4987120ce4a8
Kai Vandivier <kpvandivier@gmail.com>
2020-09-19 19:55:22 -0700

    feat: add new report definitions

--- config.js
+++ config.js
@@ -1,6 +1,24 @@
-export const BUILD_DIR = './build'
-export const SRC_DIR = './src'
+const path = require('path')
 
-export const reports = {
-    mda: { name: 'MDA Summary Report', filename: 'mda_report.js' },
+const BUILD_DIR = './build'
+const SRC_DIR = './src'
+const REPORTS_DIR = path.resolve(SRC_DIR, 'reports')
+
+exports.getEntryPath = report => path.resolve(report.dir, `${report.name}.js`)
+exports.getStylesPath = report =>
+    path.resolve(report.dir, `${report.name}.styles.css`)
+
+exports.reports = {
+    mda: {
+        title: 'MDA Treatment Summary',
+        name: 'mda',
+        dir: path.resolve(REPORTS_DIR, 'mda'),
+        specificStyles: true,
+    },
+    training: {
+        title: 'Training Summary',
+        name: 'training',
+        dir: path.resolve(REPORTS_DIR, 'training'),
+        specificStyles: false,
+    },
 }
